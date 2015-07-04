ad50b3d339effedf306544d9d4478fb4b27cfd3d
Conor McKernan <conormckernan1992@gmail.com>
2015-07-04 15:33:05 +0100

    Update config.inc.php
    Removed password details

--- QueryFiles/config.inc.php
+++ QueryFiles/config.inc.php
@@ -1,7 +1,6 @@
 <?php
 try{
-  $dbhandle = new PDO('mysql:host=127.0.0.1;dbname=FypDB','root','@isling1966');
-  $dbhandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
 }catch(PDOException $e){
     echo $e->getMessage();
     die();
